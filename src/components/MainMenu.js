c22bf0f369f7d408c0b7e4977417d1d2456b6805
PuuperttiRuma <thaapane@student.jyu.fi>
2021-05-04 16:10:38 +0300

    Reactin muistelua ja vaatimusmäärittelyä

---
+++ src/components/MainMenu.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const MainMenu = () => {
+  return (
+    <div>
+      <h1>Vaatimukset</h1>
+      <ul>      
+        <li>Treeni-appissa on mahdollista luoda oma treeniohjelma</li>
+        <li>Treeniohjelma koostuu Treeneistä, joiden aikana suoritetaan Liikkeitä</li>
+        <li>Käyttäjä itse valitsee liikkeet jotka kuuluu jokaiseen Treeniin</li>
+      </ul>
+      <h2>Use case: Tee treeni</h2>
+      <p>
+      Käyttäjä haluaa tehdä treenin. Hän painaa "tee treeni" nappulaa.
+      Appi näyttää, mikä on ensimmäinen tehtävä liike, montako niitä
+      tehdään tai kauanko. Sen alla on iso vihreä "Start" nappula.
+      <h3>Tasaisilla palautuksilla tehtävä liike</h3>
+      Kun start nappulaa painetaan kello alkaa pyöriä ja "Start"-nappi
+      muuttuu "Done!" nappulaksi. Käyttäjä tekee liikkeen ja kun liike
+      on tehty, käyttäjä painaa "Done!" nappia.
+    
+      </p>
+    </div>
+  )
+}
+
+export default MainMenu
