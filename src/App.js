9b186d8116e96df900c7788ee1d70628f7a1709b
Tommi Haapanen <thaapane@student.jyu.fi>
2021-05-08 10:50:36 +0300

    Timer laskee vain sekuntteja

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import CountdownTimer from './components/CountdownTimer.js'
 import MainMenu from './components/MainMenu.js'
 import Timer from './components/Timer.js'
 
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 
 const Timer = () => {
-  const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(58);
   const [isActive, setIsActive] = useState(false);
+  const [isCountingUp, setIsCountingUp] = useState(true);
 
   const toggle = () => {
     setIsActive(!isActive);
@@ -13,22 +13,21 @@ const Timer = () => {
     let interval = null;
     if (isActive) {
       interval = setInterval(() => {
-        if (seconds < 59){
-          setSeconds((seconds) => seconds + 1);
-        } else {
-          setSeconds(0)
-          setMinutes((minutes) => minutes + 1);
-        }        
-      }, 1000);
+        isCountingUp
+          ? setSeconds(seconds => seconds + 1)
+          : setSeconds(seconds => seconds - 1)
+      }
+      , 1000);
     } else if (!isActive && seconds !== 0) {
       clearInterval(interval);
     }
     return () => clearInterval(interval);
-  }, [isActive, seconds]);
+  }, [isActive, seconds, isCountingUp]);
 
   return (
     <div>
-      <div>{minutes}:{seconds.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false})}</div>
+      {/* {minutes}:{seconds.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false})} */}
+      <div>{seconds}</div>
       <button onClick={toggle}>{isActive ? "Pause" : "Start"}</button>
     </div>
   );
