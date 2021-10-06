059a4f8e70516df017e621cfd6751942a36a24d9
PuuperttiRuma <thaapane@student.jyu.fi>
2021-10-06 15:17:19 +0300

    Function for advancing state

--- src/components/EMOM.js
+++ src/components/EMOM.js
@@ -9,15 +9,19 @@ import Timer from './Timer.js'
 const EMOM = ({ numberOfSets, numberOfReps }) => {
     const [currentSet, setCurrentSet] = useState(1)
 
+    const goToNextSet = () => {
+        setCurrentSet(currentSet + 1)
+    }
+
     return (
         <div>
             <h2>10x10 Swings</h2>
             <h3>Set {currentSet}/{numberOfSets}</h3>
             <h4>Do {numberOfReps} reps!</h4>
-            <h4>
-                
+            <h4>                
                 <Timer isCountingUp={false} startTime={10} />
             </h4>
+            <button onClick={goToNextSet}>Next Set</button>
         </div>
     )
 }
